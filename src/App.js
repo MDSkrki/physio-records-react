ad0c63cf6d267aa7dad3edfb7fa27a8d26a6b653
Mihai Daniel Somkereki <somkerekidev@hotmail.com>
2022-05-26 15:10:04 +0200

    implemented a router and formatted code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.scss';
-import { Header } from './containers/Header/Header';
+import "./App.scss";
+import { Header } from "./containers/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
     </div>
   );
 }
