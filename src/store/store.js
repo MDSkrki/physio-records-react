3d04bb45cf93fba3421f7707d2ce360acdb8d8d0
Mihai Daniel Somkereki <somkerekidev@hotmail.com>
2022-05-29 19:01:20 +0200

    implemented changed reducer in the store as well

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./reducers/authReducer";
+import userReducer from "./reducers/userReducer";
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    user: userReducer,
   },
 });
